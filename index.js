0adfed94689a0e52a6d606ccc8ab61119221c172
Dasharath Rathod <1025rathod@gmail.com>
2025-04-15 18:46:51 +0530

    add:test rebuild

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.all('/ping', (req, res) => {
+    return res.json({
+        status: true,
+        data: req.body,
+        msg: "temporarty server is working"
+    })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
